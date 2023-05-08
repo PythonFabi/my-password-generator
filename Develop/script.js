f0e07999086abfb0237fb0e215afe2f3134234bb
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-05-07 21:29:07 -0400

    create type options and add them with concat and iterate through them

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,56 @@
 // Assignment code here
 // Create function generatePassword
-function generatePassword() {
-    var alphabeticCharacters = 'abcdefghijklmnopqrstuvwxyz'.split( '' );
-    var numericCharacters = '0123456789'.split( '' );
-    var specialCharacters = ' !()-.?[{]}_`~@#$^&*+=%|\:;"<,>/'.split( '' );
-    var lowerCase = alphabeticCharacters.toLowerCase();
-    var upperCase = alphabeticCharacters.toUpperCase();
-    var promptPasswordLength = function() {
+function generatePassword(length, lowerCaseType, upperCaseType, numericType, specialCharsType) {
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numericCharacters = '0123456789';
+  const specialCharacters = ' !()-.?[{]}_`~@#$^&*+=%|\:;"<,>/';
+  var randomPassword = '';
+   
+    // Ask user for length
+// choose length to be at least 8 and less than 128 characters
+// prompt user for length
+
+    if (lowerCaseType) {
+      randomPassword = randomPassword.concat(lowerCase);
+    };
+
+    if (upperCaseType) {
+      randomPassword = randomPassword.concat(upperCase);
+    };
+
+    if (numericType) {
+      randomPassword = randomPassword.concat(numericCharacters);
+    };
+
+    if (specialCharsType) {
+      randomPassword = randomPassword.concat(specialCharacters);
+    };
+
+    var password = "";
+    for (let index = 0; index < length; index++) {
+      const passwordIndex = Math.floor(Math.random() * randomPassword.length);
+      password += randomPassword[passwordIndex];
+    }
+    return password;
+  }
+
+    function passwordLength() {
     var choosePasswordLength = window.prompt("Choose your the length of your desired password(between 8-128 characters)");
+    choosePasswordLength = parseInt(choosePasswordLength);
     if (!choosePasswordLength) {
       return;
-    } else if (choosePasswordLength !== Number) {
-      window.alert("not a valid character length")
-    } else if (128<choosePasswordLength<8) {
-      window.alert("The password length should be between 8-128")
+    } else if (isNaN(passwordLength) || 128<choosePasswordLength<8) {
+      window.alert("not a valid password length, The password length should be between 8-128")
     } else {
-      validatePasswordLength();
+      const
     };
    };
 
 
-    var validatePasswordLength = function() {
-      var passwordArray = Array(choosePasswordLength);
-
-    }
     
-// Ask user for length
-// choose length to be at least 8 and less than 128 characters
-// prompt user for length
+    
+
   
 
 // choose types
