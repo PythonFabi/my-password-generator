9ad80edade04063ceac2b9d05b3352cbfa8b7821
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-05-08 22:25:32 -0400

    debug and make button work, and return password inside text entry field

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,13 @@
 // Assignment code here
 // Create function generatePassword
-function generatePassword(length, lowerCaseType, upperCaseType, numericType, specialCharsType) {
+function finalPassword(length, lowerCaseType, upperCaseType, numericType, specialCharsType) {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
   const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const numericCharacters = '0123456789';
-  const specialCharacters = ' !()-.?[{]}_`~@#$^&*+=%|\:;"<,>/';
+  const specialCharacters = '!().?[{]}_~@#$^&*+=%|:;"<,>';
   var randomPassword = '';
    
-    // Ask user for length
-// choose length to be at least 8 and less than 128 characters
-// prompt user for length
-
+   
     if (lowerCaseType) {
       randomPassword = randomPassword.concat(lowerCase);
     };
@@ -27,51 +24,48 @@ function generatePassword(length, lowerCaseType, upperCaseType, numericType, spe
       randomPassword = randomPassword.concat(specialCharacters);
     };
 
-    var randomPassword = "";
+    console.log(randomPassword);
+
+    var password = "";
     for (let index = 0; index < length; index++) {
       const passwordIndex = Math.floor(Math.random() * randomPassword.length);
       password += randomPassword[passwordIndex];
-    }
-    return password;
+    };
+   return password;
   }
 
-
+ // Ask user for length
+// choose length to be at least 8 and less than 128 characters
+// prompt user for length
+    function generatePassword() {
     var choosePasswordLength = prompt("Choose your the length of your desired password(between 8-128 characters)");
     choosePasswordLength = parseInt(choosePasswordLength);
-     if (isNaN(choosePasswordLength) || 8>choosePasswordLength>128) {
+     if (isNaN(choosePasswordLength) || choosePasswordLength < 8 || choosePasswordLength > 128) {
       alert("not a valid password length, The password length should be between 8-128")
     } else {
+      // choose if lowercase
       const lowerCaseType = confirm("Do you want lowercase letters in your password?");
+      // choose if uppercase
       const upperCaseType = confirm("Do you want uppercase letters in your password?");
+      // choose if numeric
       const numericType = confirm("Do you want Numbers in your password?");
+      // choose if special character
       const specialCharsType = confirm("Do you want special Characters in your password?");
     
-    if (!lowerCase && !upperCaseType && !numericType && !specialCharsType) {
+      
+// make sure to at least have one of those options
+    if (!lowerCaseType && !upperCaseType && !numericType && !specialCharsType) {
       window.alert("Not valid, choose at least one of the options available")
     } else {
-      const password = generatePassword(choosePasswordLength, lowerCaseType, upperCaseType, specialCharsType)
-      window.alert("New Password: " + password);
+      // Password should match all of the given criteria
+// password should be displayed in an alert or writtnen to that page
+      const password = finalPassword(choosePasswordLength, lowerCaseType, upperCaseType,numericType ,specialCharsType)
+     return password
     };
   };
 
+}
     
-    
-
-  
-
-// choose types
-// choose if lowercase
-// choose if uppercase
-// choose if numeric
-// choose if special character
-// make sure to at least have one of those options
-
-// Password should match all of the given criteria
-// password should be displayed in an alert or writtnen to that page
-    
- 
-
-
 
 
 // Get references to the #generate element
